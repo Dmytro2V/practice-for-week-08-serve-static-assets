9a0482488d1c3f42278815f2c23277cde57bd7e9
Dmytro2V <op2vin@gmail.com>
2023-02-07 23:53:54 +0200

    done

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const http = require('http');
 const fs = require("fs");
+const { url } = require('inspector');
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const assetsPath = './assets/'
+  if (req.url.startsWith('/static')) {
+    let urlParts = req.url.split('/');
+    
+    if (urlParts.length >= 3) {      
+      let asset;
+      let assetDir = urlParts[2]
+      let fileName = urlParts[3];
+      let fileExt = fileName.slice(fileName.indexOf('.') + 1)
+      let assetPath = assetsPath + assetDir + '/' + fileName ;
+           
+      if (fileExt.toLowerCase() === 'jpg') {
+        res.setHeader('Content-type', 'image/jpeg');
+        asset = fs.readFileSync(assetPath)
+      }
+      if (fileExt.toLowerCase() === 'css') {
+        res.setHeader('Content-type', 'text/css');
+        asset = fs.readFileSync(assetPath, 'utf-8')
+      }
+      res.statusCode = 200;
+      res.body = asset;
+      return res.end(res.body);
+    }
+
+  }
+
+
+  let indexHtml = fs.readFileSync('./index.html', 'utf-8');
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/html');
+  res.body = indexHtml;
+  res.end(res.body)
 });
 
 const port = 5000;
